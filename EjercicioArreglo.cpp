700b5f31895f4f0c7062830ad52984298f0843b9
JhonLoor <jhonloor112007@gmail.com>
2026-06-01 07:16:55 -0500

    Add EjercicioArreglo.cpp with array and vector examples

---
+++ EjercicioArreglo.cpp
@@ -0,0 +1,37 @@
+C++
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int numeros[4] = {10, 20, 30, 40};
+
+    cout << "Primer número: " << numeros[0] << endl;
+
+    for (int i = 0; i < 4; i++) {
+        cout << numeros[i] << endl;
+    }
+
+    return 0;
+}
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector<int> numeros;
+
+    numeros.push_back(10);
+    numeros.push_back(20);
+    numeros.push_back(30);
+
+    cout << "Primer número: " << numeros[0] << endl;
+
+    for (int num : numeros) {
+        cout << num << endl;
+    }
+
+    return 0;
+}
